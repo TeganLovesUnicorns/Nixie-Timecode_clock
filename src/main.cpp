e7657f0d84d41e0b7c78becf5d8f13f1c8e9eef9
Tegan R <nashvillelites@gmail.com>
2023-04-25 20:36:06 -0500

    first Commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+// Import Linear Timecode Library
+// Import RTC Library
+// Import MIDI Timecode Library
+// Import MIDI Library
+// Import NIXIE Library
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
